751248989c0856302db7885900f32f1bc730d9dc
TonySmith_2222 <xinxin134679@foxmail.com>
2022-07-12 01:25:18 +0800

    Update peer.sh

--- peer.sh
+++ peer.sh
@@ -16,10 +16,12 @@ read -p "Choose a short name for this peer: " PEER_NAME
 read -p "Enter peer ASN (e.g. 4242420817): " PEER_ASN
 read -p "Enter peer DN42 IPv4 address: " PEER_IP4
 read -p "Enter peer DN42 IPv6 address: " PEER_IP6
+read -p "Enter peer Link-local IPv6 address: " PEER_ll_IP6
 read -p "Enter peer WireGuard endpoint: " PEER_ENDPOINT
 read -p "Enter peer WireGuard pubkey: " PEER_PUBKEY
 read -p "Enter local DN42 IPv4 address: " YOUR_IP4
 read -p "Enter local DN42 IPv6 address: " YOUR_IP6
+read -p "Enter Link-local IPv6 address: " YOUR_ll_IP6
 echo
 echo "Your AS <---> AS${PEER_ASN}"
 echo "${YOUR_IP4} <---> ${PEER_IP4}"
@@ -37,6 +39,7 @@ ListenPort = 2${PEER_ASN:(-4)}
 PrivateKey = `cat private`
 
 Table = off
+PostUp = ip addr add ${YOUR_ll_IP6}/64 dev %i
 PostUp = ip addr add ${YOUR_IP6}/128 dev %i
 PostUp = ip addr add ${YOUR_IP4} peer ${PEER_IP4} dev %i
 PostUp = sysctl -w net.ipv6.conf.%i.autoconf=0
@@ -60,7 +63,7 @@ pause
 	sed -i "/dev dn42_${PEER_NAME} /d" /etc/hotplug.d/iface/40-dn42-wg
 	cat <<EOF >> /etc/hotplug.d/iface/40-dn42-wg
 [ "\$ACTION" = "ifup" -a "\$INTERFACE" = "dn42_${PEER_NAME}" ] && ip addr add ${YOUR_IP4} peer ${PEER_IP4} dev %i
-[ "\$ACTION" = "ifup" -a "\$INTERFACE" = "dn42_${PEER_NAME}" ] && ip addr add ${YOUR_IP6}/128 dev %i
+[ "\$ACTION" = "ifup" -a "\$INTERFACE" = "dn42_${PEER_NAME}" ] && ip addr add ${YOUR_ll_IP6}/64 dev %i && ip addr add ${YOUR_IP6}/128 dev %i
 EOF
 	$BATCH || vi /etc/hotplug.d/iface/40-dn42-wg || exit
 }
@@ -68,11 +71,8 @@ EOF
 mkdir -p /etc/bird/peers/
 cd /etc/bird/peers/
 cat <<EOF > dn42_${PEER_NAME}.conf
-protocol bgp ${PEER_NAME}4 from dn42 {
-    neighbor ${PEER_IP4} as ${PEER_ASN};
-}
-protocol bgp ${PEER_NAME}6 from dn42 {
-    neighbor ${PEER_IP6} as ${PEER_ASN};
+protocol bgp ${PEER_NAME} from dn42 {
+    neighbor ${PEER_ll_IP6} % 'dn42_${PEER_NAME}' as ${PEER_ASN};
 }
 EOF
 $BATCH || vi dn42_${PEER_NAME}.conf || exit
